7d7ec8cdb963c633ba3b83d30382cd9dcf54ab98
Lukas Jatmiko <lucasjatmiko@gmail.com>
2023-12-27 01:50:01 +0700

    Update migrations  - add new column id (PK) on sensor_data

--- migrations/000001_sensor_data.up.sql
+++ migrations/000001_sensor_data.up.sql
@@ -1,9 +1,11 @@
 create table if not exists sensor_data (
+    `id` bigint not null AUTO_INCREMENT,
     `id1` varchar(255) not null,
     `id2` bigint(20) unsigned not null,
     `type` varchar(255) not null,
     `timestamp` timestamp not null,
     `value` float not null,
+    PRIMARY KEY(`id`),
     constraint force_upper_case check(binary id1 = upper(id1))
 );
 
